b70621a9bafc6b1de69c0f5c5642cee866505ee5
akash <akashsreactjs@gmail.com>
2024-01-18 17:45:49 +0530

    set pingInterval to 1 sec

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ app.use(cors()).use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] },
-  pingTimeout: 1000,
+  pingTimeout: 5000,
+  pingInterval:1000
 });
 
 const connectedUsers = {}; // Keep track of connected users
