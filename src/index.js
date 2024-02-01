1833e71ae043e78f9371d766e89eb1a178487095
sayyed-nodejs <sayyedmnodejs@gmail.com>
2024-02-01 19:30:40 +0530

    fixed bugs 1st feb

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import cors from 'cors'
 import 'dotenv/config'
 import http from 'http'
 import { Server } from 'socket.io'
-import { ExpressPeerServer } from 'peer'
 
 const app = express()
 const APP_PORT = process.env.PORT
@@ -16,79 +15,59 @@ const io = new Server(server, {
   pingInterval: 1000,
 })
 
-const connectedUsers = {} // Keep track of connected users
+const availableUsers = [] // Keep track of available for call availableUsers
 
 // socket code
 io.on('connection', socket => {
   console.log('a user connected', socket.id)
 
-  socket.emit('me', { id: socket.id, name: null })
-
-  socket.on('setUserName', name => {
-    connectedUsers[socket.id] = { id: socket.id, name }
-    io.emit('onlineUsers', Object.values(connectedUsers))
-  })
-
-  // Add the user to the connected users list
-  // connectedUsers[socket.id] = { id: socket.id, name: null };
-
-  // Emit the list of online users to all clients
-  io.emit('onlineUsers', Object.values(connectedUsers))
+  socket.emit('me', { id: socket.id })
 
   socket.on('disconnect', () => {
-    console.log('user disconnected')
-    // Remove the user from the connected users list
-    delete connectedUsers[socket.id]
-
-    // // Emit the updated list of online users to all clients
-    io.emit('onlineUsers', Object.values(connectedUsers))
-    // io.emit("callEnded", { to: socket.id });
-  })
-  console.log(connectedUsers, 'connectedUsers')
-
-  socket.on('callUser', data => {
-    console.log(`callUser event from ${data.from} to ${data.name}`)
-    io.to(data.userToCall).emit('callUser', {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
-    })
-  })
-
-  socket.on('answerCall', data => {
-    console.log(`answerCall event from ${data.from} to ${data.to}`)
-    io.to(data.to).emit('callAccepted', data.signal)
-  })
-
-  socket.on('callEnded', ({ to }) => {
-    console.log(`Server: Call ended for user ${to}`)
-
-    // Notify the other user that the call has ended
-    io.to(to).emit('callEnded')
-
-    // Disconnect the other user
-    const otherSocket = io.sockets.sockets[to]
-    if (otherSocket) {
-      otherSocket.disconnect(true) // true indicates a close event
+    console.log('User disconnected:', socket.id)
+    const index = availableUsers.indexOf(socket.id)
+    if (index !== -1) {
+      availableUsers.splice(index, 1)
     }
 
-    // Disconnect the current user
-    socket.disconnect(true) // true indicates a close event
+    io.emit('availableUsers', availableUsers)
   })
 
-  socket.on('callDeclined', ({ to }) => {
-    console.log('decline')
-    io.to(to).emit('callDeclined')
+  socket.on('call', () => {
+    console.log('User is calling:', socket.id)
+    availableUsers.push(socket.id)
+
+    console.log('availableUsers', availableUsers)
+
+    if (availableUsers.length >= 2) {
+      // const randomIndex1 = Math.floor(Math.random() * availableUsers.length)
+      // const randomIndex2 = Math.floor(Math.random() * availableUsers.length)
+      const randomIndex1 = 0
+      const randomIndex2 = 1
+
+      console.log('randomIndex1, randomIndex2', randomIndex1, randomIndex2)
+
+      const user1 = availableUsers[randomIndex1]
+      const user2 = availableUsers[randomIndex2]
+
+      io.to(user1).emit('startCall', user2)
+      io.to(user2).emit('startCall', user1)
+
+      // Remove the pairedavailableUsers from the list
+      availableUsers.splice(randomIndex1, 1)
+      availableUsers.splice(
+        randomIndex2 > randomIndex1 ? randomIndex2 - 1 : randomIndex2,
+        1
+      )
+
+      io.emit('availableUsers', availableUsers)
+    }
   })
 
-  socket.on('user-connected', userID => {
-    console.log(`user-connected event for ${userID}`)
-    socket.broadcast.emit('user-connected', userID)
-  })
-
-  socket.on('user-disconnected', userID => {
-    console.log(`user-disconnected event for ${userID}`)
-    socket.broadcast.emit('user-disconnected', userID)
+  // Handle signaling
+  socket.on('signal', ({ signal, to }) => {
+    console.log('Received signal from:', socket.id, ' to:', to)
+    io.to(to).emit('signal', { signal, from: socket.id })
   })
 })
 
@@ -96,14 +75,6 @@ io.on('connection', socket => {
 app.get('/', async (req, res) => {
   res.send('Welcome to the voice calling API!')
 })
-
-// Static folder
-// const __dirpathname = path.resolve()
-// app.use('/public', express.static(path.join(__dirpathname, './public')))
-
-const peerServer = ExpressPeerServer(server, {})
-
-app.use('/peerjs', peerServer)
 
 server.listen(APP_PORT, () => {
   console.log(`Server started on port ${APP_PORT}`)
